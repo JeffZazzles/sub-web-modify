531d3648e873c3cd341c7c9bdf7138e8fa061f92
JeffZazzles <82073079+JeffZazzles@users.noreply.github.com>
2026-01-08 01:19:30 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "Private Backend by Jeff": "http://35.212.131.132:25500/",
+          "Private Backend by Jeff": "http://35.212.131.132:25500",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -1311,6 +1311,7 @@ export default {
   }
 };
 </script>
+
 
 
 
